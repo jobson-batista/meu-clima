d4a995980a6253bc74cc37e188d6f63b2eec9dde
Jobson Batista <jobson2084@gmail.com>
2024-10-12 23:51:11 -0300

    CollectionView e Celulas implementado

---
+++ Meu Clima/Color+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Color+Extensions.swift
+//  Meu Clima
+//
+//  Created by Jobson Batista on 10/12/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let constrastColor = UIColor(named: "contrastColor")
+    static let primaryColor = UIColor(named: "MainPrimaryColor")
+    static let softGrayColor = UIColor(named: "softGrayColor")
+}
